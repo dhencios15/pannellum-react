43bc80ecf4a2b18d38379c5be1cc58c7c72a361e
dhencios15 <ddelacruz@umindanao.edu.ph>
2020-12-10 21:51:39 +0800

    add pannellum;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Pannellum } from 'pannellum-react';
+import { useRef, useState } from 'react';
+import bgImage from './assets/images/bg.jpg';
 function App() {
+  const [mainImage] = useState(bgImage);
+  const pannellumRef = useRef(null);
+
+  const hanldeClickImage = (evt, args) => {
+    console.log('EVENt', evt);
+    console.log(args.name);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div>
+        <Pannellum
+          ref={pannellumRef}
+          height='850px'
+          image={mainImage}
+          pitch={10}
+          yaw={180}
+          hfov={120}
+          autoLoad
+          author={'Dhencio'}
+          title={'Fourello'}
         >
-          Learn React
-        </a>
-      </header>
+          <Pannellum.Hotspot
+            type='info'
+            pitch={11}
+            yaw={-167}
+            text='Fourello Page'
+            URL='https://www.fourello.com/'
+          />
+          <Pannellum.Hotspot
+            type='custom'
+            pitch={31}
+            yaw={150}
+            handleClick={(evt, args) => hanldeClickImage(evt, args)}
+            handleClickArg={{ name: 'test' }}
+          />
+        </Pannellum>
+      </div>
     </div>
   );
 }
